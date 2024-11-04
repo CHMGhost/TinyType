9ad19cebc59a45d8bd388b0524b3718d5e0bf30d
Minor Keith <keithminork@gmail.com>
2024-11-03 20:17:53 -0500

    Changed host ip

--- app.py
+++ app.py
@@ -257,7 +257,7 @@ def init_db():
 if __name__ == '__main__':
     init_db()
     debug_mode = os.getenv('FLASK_DEBUG', 'False').lower() == 'true'
-    host = os.getenv('FLASK_HOST', '0.0.0.0')
+    host = os.getenv('FLASK_HOST', '127.0.0.1')
     port = int(os.getenv('FLASK_PORT', 8000))
 
     app.run(host=host, port=port, debug=False)
